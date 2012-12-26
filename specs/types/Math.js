8e0d59d9253fe86245352f545e8f0edf999e1b1a
ju1ius <jules.bernable@laposte.net>
2012-12-26 04:25:43 +0100

    More specs and some jslint refactoring.

--- specs/types/Math.js
+++ specs/types/Math.js
@@ -1,20 +1,15 @@
 
 describe("Zenino.Math.mod", function()
 {
-  it("should correctly handle negative numbers modulus", function()
-  {
-    var data = [
-      // [input, expected]
-      [8, 8], [20, 8], [7, 7], [19, 7],
-      [12, 0], [24, 0],
-      [-8, 4], [-20, 4], [-7, 5], [-19, 5],
-      [-12, 0], [-24, 0]
-    ];
-    for(var i = 0, l = data.length; i < l; ++i) {
-      var params = data[i],
-          input = params[0], expected = params[1],
-          result = Zenino.Math.mod(data[i][0], 12);
-      expect(result).toEqual(expected);
-    }
-  });  
+  using('number, expected', [
+    [8, 8], [20, 8], [7, 7], [19, 7],
+    [12, 0], [24, 0],
+    [-8, 4], [-20, 4], [-7, 5], [-19, 5],
+    [-12, 0], [-24, 0]
+  ], function(input, expected) {
+    it("should correctly handle negative numbers modulus of 12", function()
+    {
+      expect(Zenino.Math.mod(input, 12)).toEqual(expected);
+    });
+  });
 });
