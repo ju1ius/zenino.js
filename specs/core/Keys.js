f292b9462727c4ae68e79e40170d5b53c81e7645
ju1ius <jules.bernable@laposte.net>
2012-12-28 06:41:43 +0100

    more specs, refactorings, and jasmine update

---
+++ specs/core/Keys.js
@@ -0,0 +1,17 @@
+describe("Zenino.Keys.getNotes", function()
+{
+  using('', [
+    ['C', ['C','D','E','F','G','A','B']],
+    ['F#', ['F#','G#','A#','B','C#','D#','E#']],
+    ['a', ['A','B','C','D','E','F','G']],
+    ['a', ['A','B','C','D','E','F','G']],
+    ['G', ['G','A','B','C','D','E','F#']],
+    ['e', ['E','F#','G','A','B','C','D']]
+  ], function(key, expected)
+  {
+    it("should return the notes in the specified key", function()
+    {
+      expect(Zenino.Keys.getNotes(key)).toEqual(expected);
+    });
+  });  
+});
